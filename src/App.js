74b6b492e933bbbe641f9d00ddca75a1e09088c2
rappcw <100694815+rappcw@users.noreply.github.com>
2022-08-18 18:21:49 +0800

    Add Dictionary component
    Add search to Dictionary component
    Create keyword state management

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer>footer here</footer>
+      </div>
+    </div>
+  );
 }
 export default App;
