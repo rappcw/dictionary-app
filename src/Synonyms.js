34bfb30fc71f324cc2d9b9cf959dee84bf950082
rappcw <100694815+rappcw@users.noreply.github.com>
2022-08-29 20:31:12 +0800

    Synonym return when present

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  return (
-    <div className="Synonyms">
-      <strong>Synonyms: </strong>
-      {props.synonyms.map(function (synonym, index) {
-        return <span key={index}>{synonym} </span>;
-      })}
-    </div>
-  );
+  if (props.synonyms.length !== 0) {
+    return (
+      <div className="Synonyms">
+        <strong className="Pink">Synonyms: </strong>
+        {props.synonyms.map(function (synonym, index) {
+          return <span key={index}>{synonym} </span>;
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
