acbb10b76445d514bb37eb4585b7f8bd7b1795bd
rappcw <100694815+rappcw@users.noreply.github.com>
2022-08-30 21:52:20 +0800

    Add photos component
    map photos - link to original image

--- src/Photos.js
+++ src/Photos.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
   console.log(props);
   if (props.photos) {
-    return <section className="Photos">Hello from photos</section>;
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-6" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    alt="images"
+                    className="img-fluid mt-4"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
   } else {
     return null;
   }
